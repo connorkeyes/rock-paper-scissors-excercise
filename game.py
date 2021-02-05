d5bfef032e43b8b257b3322c13f89387419139e7
Connor Keyes <76636011+connorkeyes@users.noreply.github.com>
2021-02-05 00:12:15 -0800

    Update game.py

--- game.py
+++ game.py
@@ -2,35 +2,32 @@
 
 import random
 
-line = "-----------------"
+line = "-------------------"
 
 username = input("Please enter your name:")
-question = input("Welcome, " + username + ", to my rock paper scissors game! Would you like to play?")
+question = input(f"Welcome, {username}, to my rock paper scissors game! Would you like to play?")
 if question == str.casefold("Yes"):
     print("Awesome! Let's do it!")
     print(line) * 2
 elif question == str.casefold("No"):
-    print("Fine, be that way then. Jerk.")
+    print("Fine, be that way. Jerk.")
     exit()
 
-while True:
-    comp_move = random.choice(["Rock", "Paper", "Scissors"])
-    inp = input("Rock, Paper, Scissors, Shoot!")
-    print("Your move:", inp)
-    print("My move:", comp_move)
-    if inp == "Rock":
-        if comp_move == "Rock":
-            print("It's a tie!")
-        if comp_move == "Paper"
-            print("You lose! Loser!")
-        if comp_move == "Scissors"
-            print("I lose! Dang it!")
-    elif inp == "Paper":
-        if comp_move == "Rock":
-            print("It's a tie!")
-        if comp_move == "Paper"
-            print("You lose! Loser!")
-        if comp_move == "Scissors"
-            print("I lose! Dang it!")
-    
-    
+comp_move = random.choice(["Rock", "Paper", "Scissors"])
+user_move = input("Rock, Paper, Scissors, Shoot!")
+print("Your move:", user_move)
+print("My move:", comp_move)
+if user_move == "Rock":
+    if comp_move == "Rock":
+        print("It's a tie!")
+    if comp_move == "Paper":
+        print("You lose! Loser!")
+    if comp_move == "Scissors":
+        print("I lose! Dang it!")
+elif comp_move == "Paper":
+    if comp_move == "Rock":
+        print("It's a tie!")
+    if comp_move == "Paper"
+        print("You lose! Loser!")
+    if comp_move == "Scissors"
+        print("I lose! Dang it!")
